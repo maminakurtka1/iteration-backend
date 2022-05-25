0d1ac52c9e8c09744ffcc9292f3d8484f791e139
maminakurtka1 <govoruhanikita@yandex.ru>
2022-05-25 17:01:27 +0300

    add to service jwt_service for generate JWT token

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type JwtClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Login route
+	e.GET("/login", login)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	if username == "jack" && password == "1234" {
+		return c.JSON(http.StatusOK, map[string]string{"username": "username", "password": password})
+	}
+	return fmt.Errorf("error authorize")
+}

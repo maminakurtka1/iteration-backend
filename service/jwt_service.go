2b787ad2769de1234ecf1b03736df29311c39ad0
maminakurtka1 <govoruhanikita@yandex.ru>
2022-05-26 16:35:21 +0300

    update jwt_service

--- service/jwt_service.go
+++ service/jwt_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -11,12 +12,37 @@ type tokenClaims struct {
 	AccountUUID string `json:"accoount_uuid"`
 }
 
+var jwtKey = []byte("my_secret_key")
+
 func GenerateToken(account_uuid string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+	claims := &tokenClaims{
+		AccountUUID: account_uuid,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
-			IssuedAt:  time.Now().Unix()},
-		AccountUUID: account_uuid})
-	// TODO: Take out key from func to .env or config file
-	return token.SignedString([]byte("key"))
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	return tokenString, err
+}
+
+func ParseToken(tokenString string) (*jwt.Token, error) {
+	claims := &tokenClaims{}
+	tkn, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	return tkn, err
+}
+
+func RefreshToken(tokenString string) {
+	claims := &tokenClaims{}
+	tkn, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
